73a8374698f490f4a2694c26452160e446885b54
Aurore Galand <agaland@shi-r7-p2.s19.be>
2025-05-20 16:44:49 +0200

    tests

--- exec.c
+++ exec.c
@@ -22,21 +22,81 @@ char	*get_env_var(char *name, char **env)
 - D'abord configurer les pipes (stdin/stdout entre les commandes)
 - Ensuite appliquer les redirections locales de chaque commande */
 
-int		execute_ast(t_command *cmd_list, t_sh *shell)
+int	init_pipes(int nb)
+{
+	int	pfd[nb][2];
+	int	i;
+
+	i = 0;
+	while (i < nb)
+	{
+		if (pipe(pfd) < 0)
+			return (1);
+	}	
+	return (0);
+}
+
+void	pipe_nb(t_command *cmd_list, int *nb)
+{
+	int	nb;
+
+	nb = 0;
+	while (cmd_list->next)
+	{
+		cmd_list = cmd_list->next;
+		nb++;
+	}
+}
+
+int		execute(t_command *cmd_list, t_sh *shell)
 {
 	int	status;
+	int	nb;
 
 	if (!cmd_list)
 		return (0);
-	while (cmd_list)
-	{
-		if (cmd_list->next)
-			status = execute_pipeline(cmd_list, shell->env);
-		else
-			status = execute_command(cmd_list, shell->env);
-	}
+	pipe_nb(cmd_list, &nb);
+	init_pipes(nb);
+	if (cmd_list->next)
+		status = execute_pipeline(cmd_list, shell->env);
+	else
+		status = execute_command(cmd_list, shell->env);
 	shell->exit_status = status;
 	return (status);
 }
 
 /* Statut de sortie : Le shell doit toujours mettre à jour et retourner le statut de la dernière commande exécutée. */
+
+
+int	apply_redirections(char *file, int pfd, int redirection)
+{
+	int	fd;
+
+	if (REDIR_IN)
+	{
+		fd = open(file, O_RDONLY);
+		if (fd < 0)
+		{
+			close(pfd[0]);
+			close(pfd[1]);
+			return (1);
+		}
+		dup2(fd, STDIN_FILENO);
+		close(fd);
+		close(pfd[0]);
+	}
+	else if (REDIR_OUT)
+	{
+		fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (fd < 0)
+		{
+			close(pfd[0]);
+			close(pfd[1]);
+			return (1);
+		}
+		dup2(fd, STDOUT_FILENO);
+		close(fd);
+		close(pfd[1]);
+	}
+	return (0);
+}
