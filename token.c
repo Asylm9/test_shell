c9782ec32e659e2aefbb73a9237b540e8c98e68d
Matthieu Goossens <magoosse@shi-r12-p2.s19.be>
2025-06-10 20:34:24 +0200

    Update_tokenizer+refacto

--- token.c
+++ token.c
@@ -6,7 +6,7 @@
 /*   By: magoosse <magoosse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 16:05:28 by magoosse          #+#    #+#             */
-/*   Updated: 2025/06/10 19:20:43 by magoosse         ###   ########.fr       */
+/*   Updated: 2025/06/10 20:33:16 by magoosse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,9 +56,94 @@ int	create_token_node(t_token **tok_lst)
 	return (SUCCESS);
 }
 
-// int	tokenize_input(t_token *tok_lst, const char *input)
-// {
-// }
+int	skip_spaces(const char *input, int *pos)
+{
+	while (input[*pos] && input[*pos] == ' ')
+		(*pos)++;
+	if (input[*pos] == '\0')
+		return (ERROR);
+	return (SUCCESS);
+}
+
+int	find_end_of_token(const char *input, int *start, int *end)
+{
+	char	quote;
+
+	while (input[(*end)] && input[(*end)] != ' ' && input[(*end)] != '|'
+		&& input[(*end)] != '<' && input[(*end)] != '>')
+		(*end)++;
+	if (input[(*start)] == '"' || input[(*start)] == '\'')
+	{
+		quote = input[(*start)++];
+		while (input[(*end)] && input[(*end)] != quote)
+			(*end)++;
+		if (input[(*end)] == '\0')
+			return (ERROR);
+	}
+	return (SUCCESS);
+}
+
+void	set_token_type(t_token *tok_lst, const char *input, int *end)
+{
+	if (input[(*end)] == '|')
+		tok_lst->type = PIPE;
+	else if (input[(*end)] == '<')
+	{
+		tok_lst->type = REDIR_IN;
+		if (input[(*end) + 1] == '<')
+		{
+			tok_lst->type = REDIR_HEREDOC;
+			(*end)++;
+		}
+	}
+	else if (input[(*end)] == '>')
+	{
+		tok_lst->type = REDIR_OUT;
+		if (input[(*end) + 1] == '>')
+		{
+			tok_lst->type = REDIR_APPEND;
+			(*end)++;
+		}
+	}
+	else
+	{
+		if (!(input[(*end)] == '\'' || !is_env_var(tok_lst->value)))
+			tok_lst->expand = EXPAND;
+	}
+}
+
+int	tokenize_input_bis(t_token *tok_lst, const char *input)
+{
+	int	start;
+	int	end;
+
+	start = 0;
+	while (input[start])
+	{
+		if (skip_spaces(input, &start) == ERROR)
+			return (ERROR);
+		end = start;
+		if (find_end_of_token(input, &start, &end) == ERROR)
+			return (ERROR);
+		tok_lst->value = ft_substr(input, start, end - start);
+		set_token_type(tok_lst, input, &end);
+	}
+}
+
+/*
+	Skip spaces at the beginning of the input.
+	If the input is empty, break the loop.
+	Find the end of the token by looking for spaces or special characters.
+	If the token starts with a quote, find the matching closing quote.
+	Create a new token with the extracted value.
+	Determine the type of the token based on the next character.
+	If the next character is a pipe, set the type to PIPE.
+	If the next character is a redirection, set the type accordingly.
+	If the next character is a word,
+		set the type to WORD and check if it needs expansion.
+	Move to the next token by skipping spaces.
+	If the next character is the end of the input, break the loop.
+*/
 
 int	tokenize_input(t_token *tok_lst, const char *input)
 {
